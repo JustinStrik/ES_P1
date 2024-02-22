17286e117e6d8f68dadbfb850d6aa2ff86ab1796
JustinStrik <strikowski.justin@gmail.com>
2024-02-21 22:02:42 -0500

    init

---
+++ AIB.py
@@ -0,0 +1,11 @@
+class lemmetry:
+    def __init__(self):
+        self.name = "testing1245"
+        self.description = "testing1245"
+        self.location = "testing"
+
+    # print
+    def print(self):
+        print(self.name)
+        print(self.description)
+        print(self.location)
